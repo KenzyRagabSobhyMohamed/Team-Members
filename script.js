11ad3c436cf46f08a20f299409d6c104be0865cd
khaled-farahat <84389471+khaled-farahat@users.noreply.github.com>
2023-11-04 17:44:16 +0200

    add hide & show sub-teams

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const membersSlider = new Swiper('.slide-content', {
-  slidesPerView:5,
+const membersSlider = new Swiper(".slide-content", {
+  slidesPerView: 5,
   spaceBetween: 25,
   grabCursor: true,
   centeredSlides: true,
-  fade:true,
+  fade: true,
   loop: true,
-  slideActiveClass: 'swiper-slide-active',
+  slideActiveClass: "swiper-slide-active",
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   breakpoints: {
@@ -27,5 +27,26 @@ const membersSlider = new Swiper('.slide-content', {
     992: {
       slidesPerView: 5,
     },
-  }
+  },
 });
+
+// select all sub-teams in the page
+const subTeams = document.querySelectorAll(".sub-team");
+
+// select all buttons in the page that have team-button class
+const buttons = document.querySelectorAll(".team-button");
+
+// add event listener to all buttons where click on it call function show
+buttons.forEach((button) => button.addEventListener("click", show));
+
+function show() {
+  const subTeamId = this.innerText.toLowerCase().split(" ").join("-");
+  // add class hide to all sub-teams
+  subTeams.forEach((subTeam) => {
+    subTeam.id !== subTeamId
+      ? subTeam.classList.add("hide")
+      : subTeam.classList.remove("hide");
+  });
+
+  // document.querySelector(`#${sectionId}`).classList.add('show');
+}
